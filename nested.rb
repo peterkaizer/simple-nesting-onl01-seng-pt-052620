20f3880ae309a1ea79b3b9c1163b1e7ec9fed83c
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 00:18:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,10 +37,8 @@ def adding_matz
      :grace_hopper => {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]},
      :alan_kay => {:known_for => "Object Orientation", :languages => ["Smalltalk", "LISP"]},
      :dennis_ritchie => {:known_for => "Unix", :languages => ["C"]},
+     :yukihiro_matsumoto = {:known_for => "Ruby", :languages => ["LISP", "C"]}
     }
-    
-    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-    
 end
 
 def changing_alan
