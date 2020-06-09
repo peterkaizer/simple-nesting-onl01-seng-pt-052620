4bdd72baacda1404798a76b043a61793ebe54f08
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 00:17:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,8 +39,7 @@ def adding_matz
      :dennis_ritchie => {:known_for => "Unix", :languages => ["C"]},
     }
     
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-programmer_hash
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
     
 end
 
