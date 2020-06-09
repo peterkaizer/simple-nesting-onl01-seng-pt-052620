a4162f1cf71ee54ead14f41a3818f7eced80d297
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 00:23:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,4 +62,6 @@ def adding_to_dennis
       :alan_kay => {:known_for => "Object Orientation", :languages => ["Smalltalk", "LISP"]},
       :dennis_ritchie => {:known_for => "Unix", :languages => ["C"]}
     }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    return programmer_hash
 end
